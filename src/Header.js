060f8cb82cdb1c2f471aeefca8d5612e06f7602b
inegg <inegg.apps@gmail.com>
2019-06-18 21:30:49 +0900

    Context를 이용하여 상태 표시하기 (이름)

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./context";
 
-const Header = () => (
-  <header>
-    <a hrer="#">Home</a> Hello, user!
-  </header>
-);
+const Header = () => {
+  const context = useContext(UserContext);
+  console.log(context);
+  return (
+    <header>
+      <a hrer="#">Home</a> Hello, {context.name}
+    </header>
+  );
+};
 
 export default Header;
---
+++ src/context.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const UserContext = React.createContext();
+
+const UserContextProvider = ({ children }) => (
+  <UserContext.Provider value={{ name: "Nicolas" }}>{children}</UserContext.Provider>
+);
+
+export default UserContextProvider;
