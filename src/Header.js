8526e1694dbf653689c40ae209146cdc36486a25
inegg <inegg.apps@gmail.com>
2019-06-18 21:19:30 +0900

    Components has been composed.

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <a hrer="#">Home</a> Hello, user!
+  </header>
+);
+
+export default Header;
---
+++ src/Screen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+
+const Screen = () => (
+  <div>
+    <Header />
+    <h1>First Screen</h1>
+  </div>
+);
+
+export default Screen;
